3eb2043a9073a154a1408761c9818e6abd749f82
Tadeusz Solarz <solarztadeus@protonmail.com>
2023-11-13 18:47:28 +0100

    Database project and basic function setup

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 import sqlite3
 import os
 
-
 HOME_PATH = os.getenv("HOME")
-if(not os.path.isdir(f"{HOME_PATH}/fini")):
-    os.makedirs(f"{HOME_PATH}/fini/")
 
-income_db = sqlite3.connect(f'{HOME_PATH}/fini/income.db')
-expenses_db = sqlite3.connect(f'{HOME_PATH}/fini/expenses.db')
+def interpreter(command):
+    # Command interpreting
+    pass
 
-command = ""
+def initialize_database(path=f'{HOME_PATH}/fini'):
+    if(not os.path.isdir(path)):
+        os.makedirs(path)
 
-while True:
-    try:
-        command = input("> ")
-    except KeyboardInterrupt:
-        print("\rQuitting...")
-        break
+    fini_db = sqlite3.connect(f'{path}/fini.db')
+    fini_cursor = fini_db.cursor()
 
-    
-    if(command == "exit"):
-        break
-    print(command)
+    fini_cursor.execute('''
+        CREATE TABLE IF NOT EXISTS INCOME (
+            id INTEGER PRIMARY KEY,
+            day,
+            month,
+            year,
+            amount INTEGER,
+            category TEXT
+        )
+        ''')
+    fini_cursor.execute('''
+        CREATE TABLE IF NOT EXISTS EXPENSE (
+            id INTEGER PRIMARY KEY,
+            day,
+            month,
+            year,
+            amount INTEGER,
+            category TEXT
+        )
+        ''')
+    fini_db.commit()
+
+    return fini_db, fini_cursor
+
+def main():
+    fini_db, fini_cursor = initialize_database()
+    command = ""
+
+    while True:
+        try:
+            command = input("> ")
+        except KeyboardInterrupt:
+            print("\rQuitting...")
+            break
+        finally:
+            if(command == "exit"):
+                break
+        
+
+if __name__ == "__main__":
+    main()
